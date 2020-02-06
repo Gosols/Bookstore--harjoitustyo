c3e9ea470a3c92f055dc51d16a42cadf7b48d528
Gosols <teemu.a.kosonen@hotmail.com>
2020-02-06 15:30:37 +0200

    Initial Commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,61 @@
+package com.example.Bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String author;
+	private String title;
+	private String isbn;
+	private int year;
+
+	public Book() {
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
